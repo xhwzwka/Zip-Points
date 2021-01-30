3d232a7d4f6642f3776b63933eeeac3df0bde29a
xhwzwka <68984335+xhwzwka@users.noreply.github.com>
2021-01-30 12:56:52 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+var zp0 = 0;
+var zp1 = 0;
+var zp2 = 0;
+
+function increment() {
+  zp0++;
+  document.getElementById("c0").innerHTML = zp0;
+}
+
+function convert() {
+  if zp0 >= 10 {
+    if zp1 >= 10 {
+      zp1 -= 10;
+      zp2++;
+      document.getElementById("c1").innerHTML = zp1;
+      document.getElementById("c2").innerHTML = zp2;
+    } else {
+      zp0 -= 10;
+      zp1++;
+      document.getElementById("c0").innerHTML = zp0;
+      document.getElementById("c1").innerHTML = zp1;
+    }
+  }
+}
