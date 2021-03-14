27bc779cc3d9c2b81e03646c9cbac22297c1f004
xhwzwka <68984335+xhwzwka@users.noreply.github.com>
2021-03-14 10:04:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 var zp0 = 0;
 var zp1 = 0;
 var zp2 = 0;
+var zp3 = 0;
 var prestigee = 0;
 
 function increment() {
-  zp0 += 1;
+  zp0++;
   document.getElementById("c0").innerHTML = zp0;
 }
 
 function convert() {
-  if (zp0 >= 10) {
-    if (zp1 >= 9) {
-      zp1 -= 10;
-      zp2 += 1;
-      document.getElementById("c1").innerHTML = zp1;
-      document.getElementById("c2").innerHTML = zp2;
+  if (zp0 > 9) {
+    if (zp1 > 9) {
+      if (zp2 > 9) {
+        zp2 -= 10;
+        zp3++;
+        document.getElementById("c2").innerHTML = zp2;
+        document.getElementById("c3").innerHTML = zp3;
+      } else {
+        zp1 -= 10;
+        zp2++;
+        document.getElementById("c1").innerHTML = zp1;
+        document.getElementById("c2").innerHTML = zp2;
+      }
     } else {
       zp0 -= 10;
       zp1 += 1;
@@ -25,7 +33,7 @@ function convert() {
 }
 
 function prestige() {
-  if (zp2 >= 1) {
+  if (zp2 > 0) {
     zp0 = 0;
     zp1 = 0;
     zp2 = 0;
@@ -34,15 +42,15 @@ function prestige() {
 }
 
 function tab(numbr) {
-  document.getElementById("tab1").style.display = "none";
-  document.getElementById("tab2").style.display = "none";
+  document.getElementById("tab0").style.display = "none";
+  document.getElementById("tab4").style.display = "none";
   document.getElementById("tab" + numbr).style.display = "inline-block";
 }
 
-while (true) {
-  if (zp2 >= 1) {
-    document.getElementById("prestigeButton").innerHTML = "Prestige for 100 P^";
+for (;;) {
+  if (zp2 > 0) {
+    document.getElementById("prestigebutton").innerHTML = "Prestige for 100 Zip Points";
   } else {
-    document.getElementById("prestigeButton").innerHTML = "Reach a ^2 to prestige";
+    document.getElementById("prestigebutton").innerHTML = "Reach ^2 to prestige!";
   }
 }
